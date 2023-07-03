4f4139d07d0141faa7928b6784907db79af43c6c
FreightDH <maxim.zolotkov@yandex.ru>
2023-07-03 18:10:37 +0300

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'import/no-mutable-exports': 'off',
+    'no-mutable-exports': 'off',
+    'no-param-reassign': 'off',
+    'no-multi-assign': 'off',
+    'no-plusplus': 'off',
+    'import/extensions': 'off',
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ]
+  },
+};
