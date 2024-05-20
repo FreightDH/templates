125b068a24a6f96bcfa7ecca5d3d47b24a7fb588
Max Zolotkov <81368535+FreightDH@users.noreply.github.com>
2024-05-20 13:39:49 +0300

    fix: update main.jsx

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from '@/app/App';
 import '@/app/styles/index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
